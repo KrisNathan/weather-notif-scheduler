6151bf09f06e51faf543b43450b25d6d459670fb
KrisNathan <kristophernathanael2005@gmail.com>
2022-12-22 16:22:35 +0700

    feat: starting message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!(
+        "Starting weather-notif-scheduler listening on: {}",
+        std::env::var("URL")?
+    );
     let rt = tokio::runtime::Runtime::new()?;
     let on_cron = move |name: &str| {
-        match rt.block_on(troll()) {
+        match rt.block_on(job()) {
             Ok(o) => println!("Job {} successfully ran: {}", name, o),
             Err(e) => eprintln!("Job {} encountered error: {}", name, e),
         };
@@ -13,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn troll() -> Result<String, Box<dyn std::error::Error>> {
+async fn job() -> Result<String, Box<dyn std::error::Error>> {
     let xd = reqwest::get(format!("{}", std::env::var("URL")?))
         .await?
         .text()
